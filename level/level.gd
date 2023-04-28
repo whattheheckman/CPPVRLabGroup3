8ee3d9fce88457e355114f458204ed23e5594963
whattheheckman <40683851+whattheheckman@users.noreply.github.com>
2023-04-27 23:25:44 -0700

    change from tabs to spaces

--- level/level.gd
+++ level/level.gd
@@ -8,71 +8,71 @@ signal replace_main_scene # Useless, but needed as there is no clean way to chec
 @onready var world_environment = $WorldEnvironment
 
 func _ready():
-	if Settings.gi_quality == Settings.GIQuality.HIGH:
-		ProjectSettings["rendering/quality/voxel_cone_tracing/high_quality"] = true
-	elif Settings.gi_quality == Settings.GIQuality.LOW:
-		ProjectSettings["rendering/quality/voxel_cone_tracing/high_quality"] = false
-	else:
-		$VoxelGI.hide()
-		$ReflectionProbes.show()
+    if Settings.gi_quality == Settings.GIQuality.HIGH:
+        ProjectSettings["rendering/quality/voxel_cone_tracing/high_quality"] = true
+    elif Settings.gi_quality == Settings.GIQuality.LOW:
+        ProjectSettings["rendering/quality/voxel_cone_tracing/high_quality"] = false
+    else:
+        $VoxelGI.hide()
+        $ReflectionProbes.show()
 
-	if Settings.aa_quality == Settings.AAQuality.AA_8X:
-		get_viewport().msaa_3d = SubViewport.MSAA_8X
-	elif Settings.aa_quality == Settings.AAQuality.AA_4X:
-		get_viewport().msaa_3d = SubViewport.MSAA_4X
-	elif Settings.aa_quality == Settings.AAQuality.AA_2X:
-		get_viewport().msaa_3d = SubViewport.MSAA_2X
-	else:
-		get_viewport().msaa_3d = SubViewport.MSAA_DISABLED
+    if Settings.aa_quality == Settings.AAQuality.AA_8X:
+        get_viewport().msaa_3d = SubViewport.MSAA_8X
+    elif Settings.aa_quality == Settings.AAQuality.AA_4X:
+        get_viewport().msaa_3d = SubViewport.MSAA_4X
+    elif Settings.aa_quality == Settings.AAQuality.AA_2X:
+        get_viewport().msaa_3d = SubViewport.MSAA_2X
+    else:
+        get_viewport().msaa_3d = SubViewport.MSAA_DISABLED
 
-	if not Settings.shadow_enabled:
-		# Disable shadows on all lights present on level load,
-		# reducing the number of draw calls significantly.
-		propagate_call("set", ["shadow_enabled", false])
+    if not Settings.shadow_enabled:
+        # Disable shadows on all lights present on level load,
+        # reducing the number of draw calls significantly.
+        propagate_call("set", ["shadow_enabled", false])
 
-	if Settings.fxaa:
-		get_viewport().screen_space_aa = 1
-	else:
-		get_viewport().screen_space_aa = 0
+    if Settings.fxaa:
+        get_viewport().screen_space_aa = 1
+    else:
+        get_viewport().screen_space_aa = 0
 
-	if Settings.ssao_quality == Settings.SSAOQuality.HIGH:
-		world_environment.environment.ssao_enabled = true
-		world_environment.environment.ssao_quality = world_environment.environment.SSAO_QUALITY_HIGH
-	elif Settings.ssao_quality == Settings.SSAOQuality.LOW:
-		world_environment.environment.ssao_enabled = true
-		world_environment.environment.ssao_quality = world_environment.environment.SSAO_QUALITY_LOW
-	else:
-		world_environment.environment.ssao_enabled = false
+    if Settings.ssao_quality == Settings.SSAOQuality.HIGH:
+        world_environment.environment.ssao_enabled = true
+        world_environment.environment.ssao_quality = world_environment.environment.SSAO_QUALITY_HIGH
+    elif Settings.ssao_quality == Settings.SSAOQuality.LOW:
+        world_environment.environment.ssao_enabled = true
+        world_environment.environment.ssao_quality = world_environment.environment.SSAO_QUALITY_LOW
+    else:
+        world_environment.environment.ssao_enabled = false
 
 #TODO: MAKE ONLY 1 BLOOM SETTING
-	if Settings.bloom_quality == Settings.BloomQuality.HIGH:
-		world_environment.environment.glow_enabled = true
-	elif Settings.bloom_quality == Settings.BloomQuality.LOW:
-		world_environment.environment.glow_enabled = true
-	else:
-		world_environment.environment.glow_enabled = false
+    if Settings.bloom_quality == Settings.BloomQuality.HIGH:
+        world_environment.environment.glow_enabled = true
+    elif Settings.bloom_quality == Settings.BloomQuality.LOW:
+        world_environment.environment.glow_enabled = true
+    else:
+        world_environment.environment.glow_enabled = false
 
-	var window_size = get_window().size
-	if Settings.resolution == Settings.Resolution.NATIVE:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-		DisplayServer.window_set_size(Vector2(1280, 720))
-		get_tree().root.set_content_scale_size(Vector2(window_size.x * 720 / window_size.y, 720.0))
-		pass
-	elif Settings.resolution == Settings.Resolution.RES_1080:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-		DisplayServer.window_set_size(Vector2(1920, 1080.0))
-		get_tree().root.set_content_scale_size(Vector2(window_size.x * 1080 / window_size.y, 1080.0))
-	elif Settings.resolution == Settings.Resolution.RES_720:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-		DisplayServer.window_set_size(Vector2(1280, 720))
-		get_tree().root.set_content_scale_size(Vector2(window_size.x * 720 / window_size.y, 720.0))
-	elif Settings.resolution == Settings.Resolution.RES_540:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-		get_tree().root.set_content_scale_size(Vector2(window_size.x * 540 / window_size.y, 540.0))
+    var window_size = get_window().size
+    if Settings.resolution == Settings.Resolution.NATIVE:
+        DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+        DisplayServer.window_set_size(Vector2(1280, 720))
+        get_tree().root.set_content_scale_size(Vector2(window_size.x * 720 / window_size.y, 720.0))
+        pass
+    elif Settings.resolution == Settings.Resolution.RES_1080:
+        DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+        DisplayServer.window_set_size(Vector2(1920, 1080.0))
+        get_tree().root.set_content_scale_size(Vector2(window_size.x * 1080 / window_size.y, 1080.0))
+    elif Settings.resolution == Settings.Resolution.RES_720:
+        DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+        DisplayServer.window_set_size(Vector2(1280, 720))
+        get_tree().root.set_content_scale_size(Vector2(window_size.x * 720 / window_size.y, 720.0))
+    elif Settings.resolution == Settings.Resolution.RES_540:
+        DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+        get_tree().root.set_content_scale_size(Vector2(window_size.x * 540 / window_size.y, 540.0))
 
 
 
 func _input(event):
-	if event.is_action_pressed("quit"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		emit_signal("quit")
+    if event.is_action_pressed("quit"):
+        Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+        emit_signal("quit")
---
+++ player/xr_gun.gd
@@ -0,0 +1,30 @@
+if Input.is_action_pressed("shoot") and fire_cooldown.time_left == 0:
+    var shoot_origin = shoot_from.global_transform.origin
+
+    var ch_pos = crosshair.position + crosshair.size * 0.5
+    var ray_from = camera_camera.project_ray_origin(ch_pos)
+    var ray_dir = camera_camera.project_ray_normal(ch_pos)
+
+    var shoot_target
+    var query : PhysicsRayQueryParameters3D = PhysicsRayQueryParameters3D.create(ray_from, ray_dir * 1000, 1, [self])
+    var col = get_world_3d().direct_space_state.intersect_ray(query) # ray_from + ray_dir * 1000
+    if col.is_empty():
+        shoot_target = ray_from + ray_dir * 1000
+    else:
+        shoot_target = col.position
+    var shoot_dir = (shoot_target - shoot_origin).normalized()
+
+    var bullet = preload("res://player/bullet/bullet.tscn").instantiate()
+    get_parent().add_child(bullet)
+    bullet.global_transform.origin = shoot_origin
+    # If we don't rotate the bullets there is no useful way to control the particles ..
+    bullet.look_at(shoot_origin + shoot_dir, Vector3.UP)
+    bullet.add_collision_exception_with(self)
+    var shoot_particle = $PlayerModel/Robot_Skeleton/Skeleton3D/GunBone/ShootFrom/ShootParticle
+    shoot_particle.restart()
+    shoot_particle.emitting = true
+    var muzzle_particle = $PlayerModel/Robot_Skeleton/Skeleton3D/GunBone/ShootFrom/MuzzleFlash
+    muzzle_particle.restart()
+    muzzle_particle.emitting = true
+    fire_cooldown.start()
+    sound_effect_shoot.play()
