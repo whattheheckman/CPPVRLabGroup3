9daf5fa60bd597bce470977046040586d83aedb8
whattheheckman <40683851+whattheheckman@users.noreply.github.com>
2023-04-27 18:19:45 -0700

    try to write a tool script, cry

---
+++ colShapeSize2x.gd
@@ -0,0 +1,25 @@
+@tool
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+		doubleSize()
+		pass
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+
+func doubleSize() -> void:
+	var tree = get_tree
+	for node in tree.get_children():
+		if node.is_class("CollisionShape3D"):
+			var shape = node.get_shape()
+			shape.set_size(shape.get_size() * 2)
+			print_debug(node + "size doubled")
+
+
