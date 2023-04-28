7a35a13d3869b70af96dc918691db19deee63aa9
whattheheckman <40683851+whattheheckman@users.noreply.github.com>
2023-04-28 04:06:14 -0700

    clean up gun code to use signals

--- player/gun/xr_gun.gd
+++ player/gun/xr_gun.gd
@@ -16,12 +16,22 @@ extends XRToolsPickable
 @onready var shoot_particle = $ShootFrom/ShootParticle
 @onready var muzzle_particle = $ShootFrom/MuzzleFlash
 
-@onready var controller := XRHelpers.get_xr_controller(self)
+
 
 func _physics_process(_delta):
+	#Laser sight
+	if raycast.is_colliding():
+		laser.global_transform.origin = raycast.get_collision_point()
+		laser.visible = true
+	else:
+		laser.visible = false
+		
+		
 
 
-	if Input.is_action_pressed("shoot") and fire_cooldown.time_left == 0:
+
+func _on_action_pressed(_pickable):
+	if fire_cooldown.time_left == 0:
 		var shoot_origin = shoot_from.position
 
 		var ch_pos = crosshair.position + Vector3(crosshair.get_frame_coords().x, crosshair.get_frame_coords().y,0) * 0.5
@@ -49,39 +59,4 @@ func _physics_process(_delta):
 		muzzle_particle.emitting = true
 		fire_cooldown.start()
 		sound_effect_shoot.play()
-
-	if raycast.is_colliding():
-		laser.global_transform.origin = raycast.get_collision_point()
-		laser.visible = true
-	else:
-		laser.visible = false
-		
-		
-func action():
-	var shoot_origin = shoot_from.position
-
-	var ch_pos = crosshair.position + Vector3(crosshair.get_frame_coords().x, crosshair.get_frame_coords().y,0) * 0.5
-	var ray_from = shoot_from.position
-	var ray_dir = xrcam.project_ray_normal(Vector2(ch_pos.x,ch_pos.y))
-
-	var shoot_target
-	var query : PhysicsRayQueryParameters3D = PhysicsRayQueryParameters3D.create(ray_from, ray_dir * 1000, 1, [self])
-	var col = get_world_3d().direct_space_state.intersect_ray(query) # ray_from + ray_dir * 1000
-	if col.is_empty():
-		shoot_target = ray_from + ray_dir * 1000
-	else:
-		shoot_target = col.position
-	var shoot_dir = (shoot_target - shoot_origin).normalized()
-
-	var bullet = preload("res://player/bullet/bullet.tscn").instantiate()
-	get_parent().add_child(bullet)
-	bullet.global_transform.origin = shoot_origin
-	# If we don't rotate the bullets there is no useful way to control the particles ..
-	bullet.look_at(shoot_origin + shoot_dir, Vector3.UP)
-	bullet.add_collision_exception_with(self)
-	shoot_particle.restart()
-	shoot_particle.emitting = true
-	muzzle_particle.restart()
-	muzzle_particle.emitting = true
-	fire_cooldown.start()
-	sound_effect_shoot.play()
+	pass # Replace with function body.
