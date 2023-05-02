cd80e9025d12317460a22362cbc11bfac666e402
whattheheckman <40683851+whattheheckman@users.noreply.github.com>
2023-05-02 03:30:55 -0700

    start player health + 2d ui elements

---
+++ player/xr_player_life.gd
@@ -0,0 +1,13 @@
+extends XROrigin3D
+@export var max_health : float = 6
+
+@onready var health = max_health
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
