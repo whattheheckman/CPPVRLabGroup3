55111564ee883211a6a38b8d9923b529086e0d3b
whattheheckman <40683851+whattheheckman@users.noreply.github.com>
2023-05-13 16:37:12 -0700

    start adding win functions

--- level/DungeonMaster.gd
+++ level/DungeonMaster.gd
@@ -25,37 +25,43 @@ var current_levers := levers_needed
 # Called when the node enters the scene tree for the first time.
 func _ready():
 
-	pass # Replace with function body.
+    pass # Replace with function body.
 
 func pulllever():
-	current_levers - 1;
-	levers_Sound.play()
-	if (current_levers <= 0):
-		doomsday()
-	
+    current_levers - 1;
+    levers_Sound.play()
+    if (current_levers <= 0):
+        doomsday()
+    
 func doomsday():
-	happymusic.stop()
-	angrymusic.start()
-	await get_tree().create_timer(2.0).timeout
-	countdown.start()
-	core_destruct_sound.play()
-	
-	for location in robotlocations:
-		var new_robot = robot
-		new_robot.health = 10
-		new_robot.set_global_position(location)
-		get_tree().root.add_child(new_robot)
-	
-	
-	
-	
-	
-	
+    happymusic.stop()
+    angrymusic.start()
+    await get_tree().create_timer(2.0).timeout
+    countdown.start()
+    core_destruct_sound.play()
+    
+    for location in robotlocations:
+        var new_robot = robot
+        new_robot.health = 10
+        new_robot.set_global_position(location)
+        get_tree().root.add_child(new_robot)
+    
+    
+    
+    
+    
+    
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
-	pass
+    
+    pass
+func _physics_process(delta):
+    while not countdown.is_stopped():
+        
 
 
 
 
+
+func _on_safezone_body_entered(body):
+    pass # Replace with function body.
