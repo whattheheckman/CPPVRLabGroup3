a8406e4e10085953ff17389b1f64068d3e60a6eb
whattheheckman <40683851+whattheheckman@users.noreply.github.com>
2023-05-13 17:01:47 -0700

    rename DialogueTrigger.gd
    used to be called ReactorDialogueTrigger

--- level/DialogueTrigger.gd
+++ level/DialogueTrigger.gd
@@ -4,13 +4,13 @@ extends Area3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+    pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+    pass
 
 
 func _on_body_entered(body): 
-	dialogue.play()
+    dialogue.play()
