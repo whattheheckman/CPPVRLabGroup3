7bf61dc29cf57500266c53380072113bc5e8fbc0
whattheheckman <40683851+whattheheckman@users.noreply.github.com>
2023-05-14 22:41:05 -0700

    make dialouge not repeat

--- level/DialogueTrigger.gd
+++ level/DialogueTrigger.gd
@@ -5,14 +5,15 @@ var has_played := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    pass
+	pass
 
 
 func _on_body_entered(body): 
-    if not has_played:
-        dialogue.play()
+	if not has_played:
+		dialogue.play()
+		has_played = true
