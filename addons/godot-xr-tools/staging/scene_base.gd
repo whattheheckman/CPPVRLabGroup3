1c671efff62ca999fbc6b78857ba1ba4e8bc7e21
whattheheckman <40683851+whattheheckman@users.noreply.github.com>
2023-05-14 22:40:28 -0700

    tabbing fix

--- addons/godot-xr-tools/staging/scene_base.gd
+++ addons/godot-xr-tools/staging/scene_base.gd
@@ -40,57 +40,57 @@ signal request_reset_scene
 ## Interface
 
 func _ready() -> void:
-    pass
+	pass
 
 # Add support for is_xr_class on XRTools classes
 func is_xr_class(name : String) -> bool:
-    return name == "XRToolsSceneBase"
+	return name == "XRToolsSceneBase"
 
 func center_player_on(p_transform : Transform3D):
-    # In order to center our player so the players feet are at the location
-    # indicated by p_transform, and having our player looking in the required
-    # direction, we must offset this transform using the cameras transform.
+	# In order to center our player so the players feet are at the location
+	# indicated by p_transform, and having our player looking in the required
+	# direction, we must offset this transform using the cameras transform.
 
-    # So we get our current camera transform in local space
-    var camera_transform = $XROrigin3D/XRCamera3D.transform
+	# So we get our current camera transform in local space
+	var camera_transform = $XROrigin3D/XRCamera3D.transform
 
-    # We obtain our view direction and zero out our height
-    var view_direction = camera_transform.basis.z
-    view_direction.y = 0
+	# We obtain our view direction and zero out our height
+	var view_direction = camera_transform.basis.z
+	view_direction.y = 0
 
-    # Now create the transform that we will use to offset our input with
-    var transform : Transform3D
-    transform = transform.looking_at(-view_direction, Vector3.UP)
-    transform.origin = camera_transform.origin
-    transform.origin.y = 0
+	# Now create the transform that we will use to offset our input with
+	var transform : Transform3D
+	transform = transform.looking_at(-view_direction, Vector3.UP)
+	transform.origin = camera_transform.origin
+	transform.origin.y = 0
 
-    # And now update our origin point
-    $XROrigin3D.global_transform = (p_transform * transform.inverse()).orthonormalized()
+	# And now update our origin point
+	$XROrigin3D.global_transform = (p_transform * transform.inverse()).orthonormalized()
 
 func scene_loaded():
-    # Called after scene is loaded
+	# Called after scene is loaded
 
-    # Make sure our camera becomes the current camera
-    $XROrigin3D/XRCamera3D.current = true
-    $XROrigin3D.current = true
+	# Make sure our camera becomes the current camera
+	$XROrigin3D/XRCamera3D.current = true
+	$XROrigin3D.current = true
 
-    # Center our player on our origin point
-    # Note, this means you can place the XROrigin3D point in the start
-    # position where you want the player to spawn, even if the player is
-    # physically halfway across the room.
-    center_player_on($XROrigin3D.global_transform)
+	# Center our player on our origin point
+	# Note, this means you can place the XROrigin3D point in the start
+	# position where you want the player to spawn, even if the player is
+	# physically halfway across the room.
+	center_player_on($XROrigin3D.global_transform)
 
 func scene_visible():
-    # Called after the scene becomes fully visible
-    pass
+	# Called after the scene becomes fully visible
+	pass
 
 func scene_pre_exiting():
-    # Called before we start fading out and removing our scene
-    pass
+	# Called before we start fading out and removing our scene
+	pass
 
 func scene_exiting():
-    # called right before we remove this scene
-    pass
+	# called right before we remove this scene
+	pass
 
 
 ## Transition to the main menu scene
@@ -101,7 +101,7 @@ func scene_exiting():
 ## Custom scene classes can override this function to add their logic, but
 ## should usually call this super method.
 func exit_to_main_menu() -> void:
-    emit_signal("request_exit_to_main_menu")
+	emit_signal("request_exit_to_main_menu")
 
 
 ## Transition to specific scene
@@ -112,7 +112,7 @@ func exit_to_main_menu() -> void:
 ## Custom scene classes can override this function to add their logic, but
 ## should usually call this super method.
 func load_scene(p_scene_path : String) -> void:
-    emit_signal("request_load_scene", p_scene_path)
+	emit_signal("request_load_scene", p_scene_path)
 
 
 ## Reset current scene
@@ -125,16 +125,16 @@ func load_scene(p_scene_path : String) -> void:
 ## logic than is performed by the brute-force scene-reload performed by
 ## staging.
 func reset_scene() -> void:
-    emit_signal("request_reset_scene")
+	emit_signal("request_reset_scene")
 
 
 # Verifies our staging has a valid configuration.
 func _get_configuration_warnings() -> PackedStringArray:
-    var warnings := PackedStringArray()
+	var warnings := PackedStringArray()
 
-    # Report missing environment
-    if !environment:
-        warnings.append("No environment specified")
+	# Report missing environment
+	if !environment:
+		warnings.append("No environment specified")
 
-    # Return warnings
-    return warnings
+	# Return warnings
+	return warnings
